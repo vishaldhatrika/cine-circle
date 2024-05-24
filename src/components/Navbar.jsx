bbd29c56eed2af8e2f892c8201b545534717753a
Vishal Dhatrika <dvishal35@gmail.com>
2024-05-24 00:51:34 -0500

    add basic components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Navbar() {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  );
+}
+export default Navbar;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Navbar from "../components/Navbar.jsx";
+
+
+function Homepage() {
+  return (
+    <div>
+      <Navbar/>
+      <h1>Homepage</h1>
+    </div>
+  );
+  
+}
+export default Homepage;
