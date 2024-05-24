5788a0dc07554ad99e835b44020b8de967342c17
Vishal Dhatrika <dvishal35@gmail.com>
2024-05-24 11:28:20 -0500

    design navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,25 @@ import ReactDOM from "react-dom";
 
 function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="navbar flex justify-between">
+      <div className=" flex logoWrapper self-center items-center">
+        <img src="logo320.png" alt="" className="h-16 w-16 inline mr-2"/>
+        <span className="ml-2 text-nowrap font-playfair font-bold text-xl">
+          Cine Circle
+        </span>
+      </div>
+
+      <div className="centerContent flex items-center">
+        <h1 className="">Navbar</h1>
+      </div>
+      
+        <ul className="navbarLinks items-center flex font-sans">
+          <li className="px-4">About</li>
+          <li className="px-4">Contact</li>
+          <li className="px-4">Login</li>
+          <li className="px-4"><a>Register</a></li>
+        </ul>
+      
     </div>
   );
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Footer from "../components/Footer.jsx";
 import Navbar from "../components/Navbar.jsx";
-
 
 function Homepage() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <h1>Homepage</h1>
+      <Footer />
     </div>
   );
-  
 }
 export default Homepage;
